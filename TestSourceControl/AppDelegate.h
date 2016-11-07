4f4f9e4d5bbc3ae1fa4cb91b82a3af9240201f5b
marcow <marcogithub@163.com>
2016-11-07 21:39:18 +0800

    Initial Commit

---
+++ TestSourceControl/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestSourceControl
+//
+//  Created by wgd on 16/11/7.
+//  Copyright (c) 2016年 marcow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestSourceControl/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestSourceControl
+//
+//  Created by wgd on 16/11/7.
+//  Copyright (c) 2016年 marcow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
